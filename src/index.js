caeb59f7ff8d17ec85fbf02b19200fd80ae51fc8
alcun <alasdair@talkwell.world>
2024-09-22 12:25:20 +0100

    Update to createroot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
